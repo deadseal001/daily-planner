dfc96044c594ec48802406a2b4794c49ff049c76
deadseal001 <liwenbo4936@gmail.com>
2022-09-12 19:47:57 -0500

    bug and button layout fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,78 +1,94 @@
-var tasks=["","","","","","","","",""];
-var loadTasks=function(){
-    tasks=JSON.parse(localStorage.getItem("dailyTasks"))
+var tasks = ["", "", "", "", "", "", "", "", ""];
 
-    if (!tasks){
-        tasks=["","","","","","","","",""];
-    };
-    for (var i=0; i<9; i++){
-        $("div[data-time="+ (i+9) +"]").text(tasks[i]);
-    };
+//function loadTasks
+var loadTasks = function () {
+  tasks = JSON.parse(localStorage.getItem("dailyTasks"));
 
-    $(".work").each(function(){
-        auditTask($(this));
-    });
+  if (!tasks) {
+    tasks = ["", "", "", "", "", "", "", "", ""];
+  }
+  for (var i = 0; i < 9; i++) {
+    $("div[data-time=" + (i + 9) + "]").text(tasks[i]);
+  }
+
+  $(".work").each(function () {
+    auditTask($(this));
+  });
 };
 
-
-$(".time-block").on("click",".work",function(){
-    var timeSlot= $(this).attr("data-time");
-    console.log(this);
-    // console.log(timeSlot);
-    var text = $("div[data-time="+timeSlot+"]").text().trim();
-    var newText= $("<textarea>").addClass("from-control col-9").attr("data-time",timeSlot).text(text);
-    console.log(newText);
-    $(this).replaceWith(newText);
-    newText.trigger("focus");
+//change timeblocks into textarea and let user input the tasks.
+$(".time-block").on("click", ".work", function () {
+  var timeSlot = $(this).attr("data-time");
+  console.log(this);
+  // console.log(timeSlot);
+  var text = $("div[data-time=" + timeSlot + "]")
+    .text()
+    .trim();
+  var newText = $("<textarea>")
+    .addClass("from-control col-9")
+    .attr("data-time", timeSlot)
+    .text(text);
+  console.log(newText);
+  $(this).replaceWith(newText);
+  newText.trigger("focus");
 });
 
-$(".saveBtn").on("click",function(){
-    var seq2=parseInt($(this).parent().attr("data-seq"))+9;
-    // console.log(seq2);
-    text=$("textarea[data-time="+seq2+"]").val();
-    // console.log($("textarea[data-time="+seq2+"]"));
-    // console.log(text);
-    tasks[parseInt($(this).parent().attr("data-seq"))]=text;
-    saveTasks();
-    var newDiv=$("<div>").addClass("col-9 work d-flex align-items-center").attr("data-time",seq2).text(tasks[parseInt($(this).parent().attr("data-seq"))]);
-    $("textarea[data-time="+seq2+"]").replaceWith(newDiv);
+//saveBtn eventlistener
+$(".saveBtn").on("click", function () {
+  var seq2 = parseInt($(this).parent().attr("data-seq")) + 9;
+  text = $("textarea[data-time=" + seq2 + "]").val();
+  tasks[parseInt($(this).parent().attr("data-seq"))] = text;
+  saveTasks();
+  var newDiv = $("<div>")
+    .addClass("col-9 work d-flex align-items-center")
+    .attr("data-time", seq2)
+    .text(tasks[parseInt($(this).parent().attr("data-seq"))]);
+  $("textarea[data-time=" + seq2 + "]").replaceWith(newDiv);
 
-    $("div.work").each(function(){
-        auditTask($(this));
-    });
+  $("div.work").each(function () {
+    auditTask($(this));
+  });
 });
 
-$(".btn").on("click",function(){
-    tasks=["","","","","","","","",""];
+//save button
+$(".btn").on("click", function () {
+  var resetBtn = confirm(
+    "Do you want to clear the Scheduler? All the task lists on this scheduler will be deleted."
+  );
+  if (resetBtn) {
+    tasks = ["", "", "", "", "", "", "", "", ""];
     saveTasks();
     loadTasks();
-})
+  };
+});
 
-var saveTasks=function(){
-    localStorage.setItem("dailyTasks", JSON.stringify(tasks));
+//save calendar to the local storage.
+var saveTasks = function () {
+  localStorage.setItem("dailyTasks", JSON.stringify(tasks));
 };
 
-function auditTask(workEl){
-    //get time from task element
-    var timeHour = parseInt($(workEl).attr("data-time"));
-    if (moment().hour() < timeHour){
-        $(workEl).removeClass("past").removeClass("present").addClass("future");
-    } 
-    else if(moment().hour() == timeHour) {
-        $(workEl).removeClass("future").removeClass("past").addClass("present");
-    } 
-    else{
-        $(workEl).removeClass("present").removeClass("future").addClass("past");
-    }
-};
-
+//function auditTask check the time and determin the bg color of the calendar.
+function auditTask(workEl) {
+  var timeHour = parseInt($(workEl).attr("data-time"));
+  if (moment().hour() < timeHour) {
+    $(workEl).removeClass("past").removeClass("present").addClass("future");
+  } else if (moment().hour() == timeHour) {
+    $(workEl).removeClass("future").removeClass("past").addClass("present");
+  } else {
+    $(workEl).removeClass("present").removeClass("future").addClass("past");
+  }
+}
 
 // loadTasks when open the page
 loadTasks();
 
-setInterval(function(){
-    console.log(moment().hour());
-    $("div.work").each(function(){
-        auditTask($(this));
+// check time and call function auditTask
+var timeH = 0;
+setInterval(function () {
+  if (timeH != moment().hour()) {
+    timeH = moment().hour();
+    $("div.work").each(function () {
+      auditTask($(this));
     });
+  }
 }, 1000);
